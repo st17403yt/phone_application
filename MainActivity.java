25c467e2e218ee8666c38425a87fb60f2e791847
st17403yt <72368680+st17403yt@users.noreply.github.com>
2021-05-07 15:21:00 +0900

    さいしょ

---
+++ MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void click1(View view) {
+        TextView textView = (TextView) findViewById(R.id.TextView1);
+        textView.setText("1");
+    }
+
+    public void clickButton(){
+
+    }
+}
